f51ec053f38315e28550b5b297d679cd25586262
Sinooonn <116568330+Sinooonn@users.noreply.github.com>
2022-11-22 08:30:17 +0800

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,18 @@
+class Dog extends Animal {
+
+  
+  void eat()
+  {
+   System.out.print("Dogs love to eat chciekn and ");
+  }
+  
+  void sleep()
+  {
+   System.out.println("sleep 8 hours a day");
+  }
+  
+  void makeSound()
+  {
+   System.out.println("Pa take po ng exam :))");
+  }
+}
